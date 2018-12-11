e2c80917089c8d84cac74ff89e9138373293d938
Jonathan Sidi <yonis@metrumrg.com>
2018-12-10 19:12:10 -0500

    add alignment and spacing

--- R/clean_ctl_addin.R
+++ R/clean_ctl_addin.R
@@ -1,7 +1,9 @@
 #' @importFrom rstudioapi getSourceEditorContext navigateToFile
 #' @importFrom tools file_path_sans_ext file_ext
 clean_ctl_addin <- function(){
+  
   adc <- rstudioapi::getSourceEditorContext()
+  
   file_name <- basename(adc$path)
   
   tf <- sprintf('%s_clean.%s',tools::file_path_sans_ext(file_name),tools::file_ext(file_name))
--- R/ctl_style.R
+++ R/ctl_style.R
@@ -6,8 +6,47 @@ ctl_style <- function(x){
   x4 <- gsub('^\\s+','',gsub('\\s+',' ',x3))
   x4[grepl('^[$]',x4)] <- sub('\\s','\n  ',x4[grepl('^[$]',x4)])
   x4[!grepl('^[$]',x4)] <- sprintf('  %s',x4[!grepl('^[$]',x4)])
-  x5 <- paste0(x4,collapse = '\n')
-  ret <- gsub('\\n[$]','\n\n$',x5)
+  x_space <- ctl_space(x4)
+  x_align <- ctl_align(x_space)
+  x_collapse <- paste0(x_align,collapse = '\n')
+  x_return <- gsub('\\n[$]','\n\n$',x_collapse)
+  x_return
+}
+
+ctl_space <- function(x){
+  x <- gsub('\\s+[=]|[=]\\s+','=',x)
+  x <- gsub('=',' = ',x)
+  x <- gsub('\\s+[,]|[,]\\s+','=',x)
+  x <- gsub(',',', ',x)
+  x <- gsub('\\s+[;]|[;]\\s+',';',x)
+  gsub(';',' ; ',x)
+}
+
+ctl_align <- function(x){
+  idxs <- grep('^\\$',x)
+  y <- vector('numeric',length(x))
+  y[idxs] <- 1 
+  y <- cumsum(y)
   
-  ret
+  y1 <- lapply(split(x,y),function(x){
+    x <- align(x,char = '=')
+    x <- align(x,char = ';')
+    x
+  })
+  
+  unlist(y1,use.names = FALSE)
+  
+}
+
+align <- function(x,char = '='){
+  ydx <- regexpr(char,x)
+  if(all(ydx==-1))
+    return(x)
+  idx_eq <- which(ydx!=-1)
+  new_eq <- rep(char,length(ydx))
+  new_eq[idx_eq] <- sprintf('%s%s',strrep(' ',max(ydx[idx_eq]) - ydx[idx_eq]),char)
+  for(i in 1:length(ydx)){
+    x[i] <- sub(char,new_eq[i],x[i])
+  }
+  x
 }
